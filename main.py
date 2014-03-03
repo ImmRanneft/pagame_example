2ce36fab9386278603a82fd1600a5f156ec76597
Den <ranneft.dv@molodost.bz>
2014-03-03 19:01:39 +0400

    new_translator

--- main.py
+++ main.py
@@ -12,30 +12,44 @@ from slg.map.map import Map
 from slg.renderer import Renderer
 from slg.map.layer import Layer
 
-BACKGROUND_COLOR = "#004400"
-
+PAUSED = 0
+RUNNING = 1
 
 def main():
+
     config = configparser.ConfigParser()
     config.sections()
     config.read(os.path.join(os.getcwd(), "config", "main.ini"))
+
+    """
+        map_config
+    """
     l_map = config['MAIN']['map']
+
+    """
+        display config
+    """
     config_display = config['DISPLAY']
     display_width = int(config_display.get('width', 800))
     display_height = int(config_display.get('height', 600))
-    fullscreen = int(config_display.get('fullscreen', 0))
-    pygame.init()
-
     display_tup = (display_width, display_height)
+
+    fullscreen = int(config_display.get('fullscreen', 0))
     display_flags = 0
     if fullscreen == 1:
         display_flags |= FULLSCREEN
         videoinfo = pygame.display.Info()
         display_tup = (videoinfo.current_w, videoinfo.current_h)
 
+    clr = (0, 0, 0)
+
+    pygame.init()
     display = pygame.display.set_mode(display_tup, display_flags)
 
-    clr = (0, 0, 0)
+    paused_surface = pygame.Surface(display_tup)
+    paused_surface.set_alpha(128)
+    paused_surface.fill(clr)
+
     clock = pygame.time.Clock()
 
     camera = Camera(display_tup)
@@ -50,34 +64,47 @@ def main():
     camera.reset_camera_to((0, 0))
 
     running = True
-
+    state = RUNNING
     while running:
+        previous_state = state
         for e in pygame.event.get():
+            if state != PAUSED:
+                if e.type == KEYDOWN:
+                    if e.key == K_DOWN:
+                        camera.set_moving_y(camera.MOVEMENT_POSITIVE)
+                    if e.key == K_UP:
+                        camera.set_moving_y(camera.MOVEMENT_NEGATIVE)
+                    if e.key == K_RIGHT:
+                        camera.set_moving_x(camera.MOVEMENT_POSITIVE)
+                    if e.key == K_LEFT:
+                        camera.set_moving_x(camera.MOVEMENT_NEGATIVE)
+                if e.type == KEYUP:
+                    if e.key == K_UP or e.key == K_DOWN:
+                        camera.set_moving_y(camera.MOVEMENT_STOP)
+                    if e.key == K_LEFT or e.key == K_RIGHT:
+                        camera.set_moving_x(camera.MOVEMENT_STOP)
+                    if e.key == K_c:
+                        camera.reset_camera_to((world_map.get_world_center()))
             if e.type == KEYDOWN:
                 if (e.key == K_F4 and pygame.key.get_mods() and pygame.KMOD_ALT) or e.key == K_ESCAPE:
                     running = False
-                if e.key == K_DOWN:
-                    camera.set_moving_y(camera.MOVEMENT_POSITIVE)
-                if e.key == K_UP:
-                    camera.set_moving_y(camera.MOVEMENT_NEGATIVE)
-                if e.key == K_RIGHT:
-                    camera.set_moving_x(camera.MOVEMENT_POSITIVE)
-                if e.key == K_LEFT:
-                    camera.set_moving_x(camera.MOVEMENT_NEGATIVE)
             if e.type == KEYUP:
-                if e.key == K_UP or e.key == K_DOWN:
-                    camera.set_moving_y(camera.MOVEMENT_STOP)
-                if e.key == K_LEFT or e.key == K_RIGHT:
-                    camera.set_moving_x(camera.MOVEMENT_STOP)
-                # if e.key == K_c:
-                #     zz = layer.get([0, 0])
-                #     camera.reset_camera_to((zz.get_x(), zz.get_y()))
+                if e.key == K_p:
+                    if state == PAUSED:
+                        state = RUNNING
+                        print('running')
+                    else:
+                        state = PAUSED
+                        print('paused')
 
-        camera.update()
-        display.fill(clr)
-        visible_area = camera.get_bounds()
-        world_map.draw(visible_area)
-
+        if state == RUNNING:
+            display.fill(clr)
+            visible_area = camera.get_bounds()
+            world_map.draw(visible_area)
+            camera.update()
+        elif state == PAUSED:
+            if previous_state != state:
+                display.blit(paused_surface, (0, 0))
         pygame.display.update()
         clock.tick(30)
         # running = False
